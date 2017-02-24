6c9817927ab4d2cdb73918e2a8581b99597e4947
Jack <j.gumoes@gmail.com>
2017-02-24 23:45:40 +0000

    made a triangle that rotates on every update

--- 314.py
+++ 314.py
@@ -8,24 +8,30 @@ Created on Fri Feb 24 20:43:04 2017
 """
 
 import pyqtgraph as pg
-import time
+import numpy as np
 
 app = pg.mkQApp()
 plt = pg.plot()
 plt.setYRange(0, 500)
 plt.setXRange(0, 500)
 
+# make a triangle
+x = [200, 300, 400, 200, 200, 200]
+y = [200, 200, 200, 400, 300, 200]
+tri = np.matrix([x, y])
+x, y = tri.getA()
+plt.plot(x, y)
 
-import random
 
 def update():
-    x = []
-    y = []
-    for i in range(500):
-        x.append(random.randrange(500))
-        y.append(random.randrange(500))
+    global tri
+    rot = np.matrix([[0, 1], [-1, 0]])
+    tri = 250.5 + rot.dot((tri-250.5))
+    x, y = tri.getA()
     plt.plot(x, y, clear=True)
-    #time.sleep(1)
+    
+    if not plt.isVisible():
+        timer.stop()
 
 timer = pg.QtCore.QTimer()
 timer.timeout.connect(update)
