b7e2232ea592369e425b55f241da8c02006bb651
Jack <j.gumoes@gmail.com>
2017-02-24 23:49:11 +0000

    made a the triangle into a square. stays in place as it rotates ie. rotation works good

--- 314.py
+++ 314.py
@@ -15,19 +15,19 @@ plt = pg.plot()
 plt.setYRange(0, 500)
 plt.setXRange(0, 500)
 
-# make a triangle
-x = [200, 300, 400, 200, 200, 200]
-y = [200, 200, 200, 400, 300, 200]
-tri = np.matrix([x, y])
-x, y = tri.getA()
+# make a square
+x = [1, 1, 500, 500]
+y = [1, 500, 500, 1]
+sq = np.matrix([x, y])
+x, y = sq.getA()
 plt.plot(x, y)
 
 
 def update():
-    global tri
+    global sq
     rot = np.matrix([[0, 1], [-1, 0]])
-    tri = 250.5 + rot.dot((tri-250.5))
-    x, y = tri.getA()
+    sq = 250.5 + rot.dot((sq-250.5))
+    x, y = sq.getA()
     plt.plot(x, y, clear=True)
     
     if not plt.isVisible():
