205171738393ee0d41d23522b493dee6110a07bd
Jack Gumoes <j.gumoes@gmail.com>
2018-10-11 23:37:03 +0300

    I goofed the filenames. This version is the recent, Tikhonov inspired version. Except I have no idea what in the fuck I've done with the coordinate data structure, and the area calculation seems unnecesarilly inefficient. Might replace it with trapezium-rule

--- 314.py
+++ 314.py
@@ -1,150 +1,148 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 24 20:43:04 2017
+Created on Sun Jun 17 15:16:26 2018
 
 @author: Jack
 
-2nd attempt at project euler question 314
+project euler problem 314
 """
 
-import pyqtgraph as pg
 import numpy as np
-import threading
 
-app = pg.mkQApp()
-plt = pg.plot()
-plt.setYRange(1, 500)
-plt.setXRange(1, 500)
+def make_P_init(n):
+    """makes an initial set of P with n values"""
+    x = np.linspace(0, 250, n+2)[1:-1]
+    y = x*0 + 250
+    return np.vstack((x, y)).T
 
-# make the top sector
-y = 500*np.ones(250, dtype="int")
-x = np.append(np.array(range(1, 501)), np.array(range(1, 501))[::-1])
-x = np.append(x, 1)
-#plt.plot(y)
-
-def diff(vals):
-    """finds second order difference of vals.
-    analogous to second-order differentiation"""
-    return vals[0] + vals[2] - 2*vals[1]
-
-def ratio(top, bot=None, end=False):
-    """finds area/perimeter ratio between top and bottom lines"""
-    if bot == None:
-        bot = 501-top
-    val = top-250.5 #bot
-    i = 0
-    area = (2*np.sum(val) - val[0] - val[-1])
-    per = 0
-    while i < len(val)-1:
-        #area += (val[i]+val[i+1])/2
-        per += np.sqrt(1+ (top[i+1]-top[i])**2.)
-        i+=1
-    per += val[0]
-    #return area, per, area/per
-    return area/per
-
-def fudge(i=0, inc=1):
-    """fudges a point down (or up) to maximise it's ratio.
-    i is the location of the corner"""
-    #y_int = np.asfarray(y[0: i+2])
-    global y
-    # homebrew optimisation using increments instead of dumb scipy that i can't figure out
-    #inc = 1  # the increment of the corner.
-    while 501 > y[i] > 250:
-        #print inc
-        y_int = np.copy(y)
-        y_int[i] -= inc # there isn't a better way of doing this
-        if ratio(y) < ratio(y_int):
-            y = y_int
-            global last, mirr
-            last = i
-            mirr = 0
-#        elif inc == 1:
-#            inc = -1
-        else:
-            break
-
-def mirror():
-    """mirrors y along the axis y=-x ie. diagonally"""
-    global mirr
-    mirr += 1
-    #pg.plot(501-y[::-1], 250+x[:len(y)])
-#    z_int = 501-y[::-1]
-    z = 501-y[::-1]
-#    for i in z_int:
-#        z.append(round(i))
-    i = 0
-    while i < len(z):
-        if z[i+1]>z[i]:
-            break
-        i+=1
-    z = z[i:]
-    w = 250+x[i:len(y)]
+def lengths(P):
+    """outputs the lengths of each line in the segment, given coordinates P
+    P0 is (x0, 250), and Pe is the last coordinate before the line of symmetry"""
+    L = np.zeros(len(P)+1) # L = lengths of each line
+    L[0] = P[0][0]
+    xe, ye = P[-1]
+    L[-1] = np.sqrt((ye**2 + xe**2 - 2*ye*xe)/2)
     
-    out = np.ones(len(y), dtype="int")*500
-    i = 0
-    while i < len(z)-1:
-        out[z[i]-1:z[i+1]] = np.linspace(w[i], w[i+1], 1+z[i+1]-z[i], dtype='int')
-        i+=1
+    if len(P) == 1:
+        return L
     
-    i = 0  # third's the charm
-    while i < len(out):
-        out[i] = round(out[i])
-        i += 1
-    return out
-
-break_var = True
-lap = 0
-inc = 80
-
-mirr = 0
-last = -1
-def run():
-    global break_var, lap, last, inc
-    while break_var:
-        i = 0
-        lap = 0
-        while i < 250:
-            if i == last or (last == -1 and i>0):
-                global y
-                y = mirror()
-                print "lapped"
-                last = -1
-#                break_var = False
-#                break
-            if mirr > 2:
-                print "mirror lapped"
-                inc -= 1
-                if inc < 1:
-                    break_var = False
-                    break
-            fudge(i, inc)
-            i+=1
-            if not plt.isVisible():
-                break_var = False
-                print "plot closed"
-                break
-    timer.stop()
-    return "it's over"
-            
-
-def update():
-    top = np.append(y, y[::-1])
-    top = np.append(top, 501-top)
-    top = np.append(top, top[0])
-    plt.plot(x, top, clear=True)
-    if not plt.isVisible():
-        timer.stop()
-        global break_var
-        break_var = False
+    dP = (P[1:] - P[:-1]).transpose()
+    L[1:-1] = np.sqrt(dP[0]**2 + dP[1]**2)
+    return L
 
 
-timer = pg.QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(50)
+def area(P, L):
+    """the area under the segment, given coordinate P
+    Finds the area using trigonometry
+    This is 1/8 of the true area"""
+    A = np.zeros(len(P) + 1) # areas of each triangle segment
+    #PT = P.transpose()
+    
+    A[0] = P[0][0]*125
+    A[-1] = L[-1]*np.sqrt(2)*(P[-1][0]+P[-1][1])/4
+    
+    P = P.flatten()
+    
+    if len(P) == 1:
+        return A
+    
+    R = np.sqrt(np.sum(P**2, 1)) # distance of each point from the center
+    S = (R[1:] + R[:-1] + L[1:-1])/2
+    
+    A[1:-1] = np.sqrt(S*(S-R[1:])*(S-R[:1])*(S-L[1:-1]))
+    return A
 
-#thread = threading.Thread(target=run)
-#thread.start()
-#thread.join()
-#timer.stop()
-run()
+
+def ratio(P):
+    """Returns the ratio of area to perimeter"""
+    L = lengths(P)
+    A = area(P, L)
+    return np.sum(A)/np.sum(L)
+
+def update(P, F, F_l, X_l):
+    """Updates the value of P using gradient descent"""
+    # reshape P into a vector array of only the variables
+    f_P = P.flatten()
+    X = np.zeros(len(f_P)-1)
+    X[0] = f_P[0]
+    X[1:] = f_P[2:]
+    
+    #F = -ratio(P)  # the current function value
+    
+    fd = f_d(X) - F
+    bd = F - b_d(X)
+    
+    # finding the Jacobian matrix i.e. grad(-ratio)
+    # it looks messy as hell, but it's faster than using for loops
+    ineq = (fd>=0)*(bd<=0)    # element=1 if point is a minima
+    float_J = ((fd+bd)/2)*(1-ineq)  # stops non-zero derivatives appearing at minima
+    y = np.sum((X - X_l)*(F - F_l))/np.sum((F - F_l)**2) #  Barzilai-Borwein
+    J = np.int(float_J*y)
+    J += (float_J!=0)*(J==0)*1*np.sign(float_J)
+    #print(J, ineq, fd, bd)
+    
+    X = X - J
+    lim = X > 250   # =1 if element is over 250 (out of bounds)
+    X = X*(1-lim) + 250*lim
+    #print(X)
+    P = (np.insert(X, 1, 250)).reshape((int(len(X)+1/2), 2))
+    
+    return P, np.max(J**2)
+
+def update0(P, F):
+    """a function for the first iteration. is needed because the gamma varibale
+    is found using the gradient of the last iteration, which obviously doesn't
+    exist for the first one"""
+    # reshape P into a vector array of only the variables
+    f_P = P.flatten()
+    X = np.zeros(len(f_P)-1)
+    X[0] = f_P[0]
+    X[1:] = f_P[2:]
+    
+    #F = -ratio(P)  # the current function value
+    
+    fd = f_d(X) - F
+    bd = F - b_d(X)
+    
+    # finding the Jacobian matrix i.e. grad(-ratio)
+    # it looks messy as hell, but it's faster than using for loops
+    ineq = (fd>=0)*(bd<=0)    # element=1 if point is a minima
+    float_J = ((fd+bd)/2)*(1-ineq)  # stops non-zero derivatives appearing at minima
+    
+    J = np.sign(float_J)
+    #print(J, ineq, fd, bd)
+    
+    X = X - J
+    P = (np.insert(X, 1, 250)).reshape((int(len(X)+1/2), 2))
+    
+    return P, np.max(J**2)
+    
+def f_d(X):
+    """calculates the forward derivatives given X"""
+    f_X = np.eye(len(X)) + X
+    f_F = np.zeros(len(X))
+    for v in range(0, len(f_X)):
+        i = f_X[v]
+        P = (np.insert(i, 1, 250)).reshape((int((len(i)+1/2)/2), 2))
+        f_F[v] = -ratio(P)
+    return f_F
+
+def b_d(X):
+    """calculates the forward derivatives given X"""
+    f_X = X - np.eye(len(X))
+    f_F = np.zeros(len(X))
+    for v in range(0, len(f_X)):
+        i = f_X[v]
+        P = (np.insert(i, 1, 250)).reshape((int((len(i)+1/2)/2), 2))
+        f_F[v] = -ratio(P)
+    return f_F
+
+def opt(P):
+    """optimises P to maximise ratio, using update()"""
+    Ra = ratio(P)
+    #cost = 10
+    P, cost = update0(P, -Ra)
+    while cost > 0: # there's a chance this might osciallate around the answer
+        P, cost = update(P, -Ra)
+        Ra = ratio(P)
+        print(Ra, P, cost)
