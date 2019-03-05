63e62f7182a392a71599eec6de70876da2915281
Jack Gumoes <j.gumoes@gmail.com>
2019-03-05 21:35:26 +0000

    c_shuffle is tested and ready to replace a lot of the heavy lifting in the python script

--- C code/c_shuffle.c
+++ C code/c_shuffle.c
@@ -3,3 +3,214 @@
 // I might put other stuff in here if the main script still isn't fast enough,
 // but I don't think I will.
 
+#include <math.h>
+#include <stdio.h>
+
+
+double find_ratio(int fP[], int len_P){
+    // the original find_ratio_flat only accepts a numpy array, but it was easiest to make add_bin add straight to an existing array
+    // so, I split off the bulk of find_ratio_flat into this function, which accepts a C array, making life way easier while still keeping
+    // find_ratio_flat but without bloating the dll
+    /*
+    double find_ratio_flat(){    //this line is for testing the body of code
+
+    int len_P = 9;
+    int fP[] = {123, 135, 146, 158, 168, 178, 188, 198, 207, 249, 247, 244, 240, 235, 230, 223, 215};
+    */
+
+    int pX[len_P];
+    for(int pos_count = 0; pos_count<len_P; pos_count++){
+        pX[pos_count] = fP[pos_count];
+    }
+    int pY[len_P];
+    pY[0] = 250;
+    for(int pos_count = 0; pos_count<len_P-1; pos_count++){
+        pY[pos_count+1] = fP[pos_count+len_P];
+    }
+
+    /*
+    //testing
+    for(int i = 0; i < 9; i++){
+        printf("%d\t%d\n", pX[i], pY[i]);
+    }
+    */
+
+    /* find lengths */
+    int no_i = (pow(pY[len_P-1]-pX[len_P-1], 2) > 0);
+    
+    double L[len_P+1];  // if no_i is false, pretend like this has len_P not len_P+1
+    // Todo: L doesn't need to exist. get rid of it and add the lengths
+    // directly to perimeter
+    
+    L[0] = fabs((double) pX[0]*8);
+
+    int over_1 = (len_P != 1);
+    int dPX[len_P];
+    int dPY[len_P];
+    if(len_P > 1){
+        //Todo: fold these two for loops together and get rid of
+        //dPX and dPY
+        for(int i = 0; i < len_P-1; i++){
+            dPX[i] = pX[i+1] - pX[i];
+            dPY[i] = pY[i+1] - pY[i];
+        }
+        for(int i = 0; i < len_P-1; i++){
+            int X = dPX[i]; int Y = dPY[i];
+            L[i+1] = sqrt(64*(X*X + Y*Y));
+        }
+    }
+
+    /*
+    //testing
+    for(int i = 0; i < 9; i++){
+        //printf("%d\t%d\n", dPX[i], dPY[i]);
+        //printf("%d\n", pX[i+1]-pX[i]);
+        printf("%E\n", L[i]);
+    }
+    */
+    
+    double perimeter = 0;
+    for(int i = 0; i < len_P; i++){
+        perimeter += L[i];
+    }
+    //printf("%E\n", perimeter);
+    if(no_i==1){
+        int Y  = pY[len_P-1];
+        int X = pX[len_P-1];
+        //L[len_P] = sqrt((double) (Y*Y + X*X - 2*Y*X)*32);  
+        perimeter += sqrt((double) (Y*Y + X*X - 2*Y*X)*32);
+    }
+    //printf("%E\n", perimeter);
+
+    /*find area*/
+    int xe[len_P*2 + 2];
+    int ye[len_P*2 + 2];
+    ye[0] = 250;
+    xe[len_P*2 + 1] = 250;
+
+    xe[0] = 0;
+    ye[len_P*2 + 1] = 0;
+    
+    for(int i = 0; i<len_P; i++){
+        xe[i+1] = pX[i];
+        xe[i+len_P+1] = pY[len_P-1-i];
+
+        ye[i+1] = pY[i];
+        ye[i+len_P+1] = pX[len_P-1-i];
+    }
+
+    /*
+    //testing
+    for(int i = 0; i<20; i++){
+        printf("%d\t%d\n", xe[i], ye[i]);
+    }
+    for(int i = len_P-1; i>=0; i--){
+        //printf("%d\n", pY[-i+len_P-1]);
+    }
+    for(int i = 0; i<len_P; i++){
+        int i2 = -i;
+        //printf("%d\t%d\n", i2+len_P-1, pY[i2+(len_P-1)]);
+        //printf("%d\n", pY[i]);
+    }
+    */
+
+    long area = 0;
+    for(int i = 0; i <= len_P*2 - 1; i++){
+        area += (xe[i+1]-xe[i])*(ye[i+1]+ye[i]);
+        //printf("%d\n", area);
+    }
+    area = area*2;
+    //printf("%d\n", area);
+
+    /*find ratio*/
+    double ratio = (area/perimeter);
+    //printf("%f\n", ratio);
+    return(ratio);
+}
+
+double find_ratio_flat(const void * flat_P, int len_P){
+    // same function as in the main script, ported to C
+    // accepts a flattened P as a numpy array, same as
+    // the function in the main script
+    // len_P is the number of coordinates in P,
+    // so there would be len_P x values and len_P-1 y values
+    // in flat_P
+    // note: find_ratio_flat only accepts a numpy array, but it was easiest to make add_bin add straight to an existing array
+    // so, I split off the bulk of find_ratio_flat into find_ratio function, which accepts a C array, making life way easier while still keeping
+    // find_ratio_flat but without bloating the dll
+    int * fP = (int *) flat_P;
+    return(find_ratio(fP, len_P));
+}
+
+void add_bin(int flat_P[], int num, int arr_len){
+	// add 1 to elements in array flat_P. position of 1s is selected by num. flat_P is the pointer to an array of length arr_len
+    // this function was copied over from c_functions, but I cut some corners to speed things up and save on memory
+    // and yes, i do need that memory, because firefox is using 3GB right now and i'll be damned if i close it
+
+	for(int i=0; i<arr_len; i++){
+		int n2 = pow(2, arr_len-i-1);
+		if(num - n2 < 0){
+			//bin_arr[i] = 0;
+		}
+		else{
+			num = num - n2;
+			flat_P[i] += 1;
+		}
+	}
+}
+
+double scoot_C(int flat_P[], int num, int len_P){
+    // adds num to flat_P (i.e. calls add_bin), then returns the ratio of the new array.
+    // accepts flat_P as a C array. this is intended to be called a shuffling function within C
+    // turns out, though, it accepts numpy arrays just fine.
+    int new_fP[len_P*2];
+    for(int i = 0; i < len_P*2-1; i++){
+        new_fP[i] = flat_P[i];
+        //printf("%d\n", new_fP[i]);
+    }
+    //add_bin(flat_P, num, len_P);
+    return(find_ratio(new_fP, len_P));
+}
+
+double scoot_np(const void * flat_P, int num, int len_P){
+    // just a wrapper for scoot_C that allows it to be called from python with numpy arrays.
+    // it's mainly here for testing, and just in case I can't be arsed to do shuffle_C
+    // actually, this doesn't function any different from scoot_C
+    // which is actually pretty good, because scoot_C is obviously a better name than scoot_np
+    int * fP = (int *) flat_P;
+    return(scoot_C(fP, num, len_P));
+}
+
+void shuffle_C(const void * flat_P, int len_P, int num_start, int num_end){
+    // completely replaces the shuffle function in the python script
+    // accepts a numpy array and the length of P, calls scoot_C for every
+    // value between num_start and num_end, checking if the current ratio is larger
+    // than the previous one. it returns the largest ratio and the number associated with the
+    // largest ratio.
+    // the intention with this function is that python will call it in several processes,
+    // hopefully making multi-processing much easier
+    // PROTIP: imagine the underscore in shuffle_C is a comma, and also it's pronounced with a transatlantic accent.
+    // it makes the code a lot more entertaining
+
+
+}
+
+void main(){
+    // function for testing other functions
+    int len_P = 9;
+    int flat_P[] = {123, 135, 146, 158, 168, 178, 188, 198, 207, 249, 247, 244, 240, 235, 230, 223, 215};
+    
+    //printf("%E", find_ratio_flat());
+    //printf("%f\n", find_ratio_flat(flat_P, len_P));
+
+    /*
+    int flat_P_OG[] = {123, 135, 146, 158, 168, 178, 188, 198, 207, 249, 247, 244, 240, 235, 230, 223, 215};
+    add_bin(flat_P, 9, len_P);
+    for(int i =0; i<len_P; i++){
+        printf("%d\t%d\n", flat_P_OG[i], flat_P[i]);
+    }*/
+
+    printf("%f\n", scoot_C(flat_P, 18, len_P));
+    printf("%f\n", scoot_np(flat_P, 18, len_P));
+}
+
--- C code/compile_dll.bat
+++ C code/compile_dll.bat
@@ -1,4 +1,5 @@
 @echo off
 title Compile DLL
 gcc -fPIC -shared -o c_functions.dll c_functions.c
+gcc -fPIC -shared -o c_shuffle.dll c_shuffle.c
 pause
